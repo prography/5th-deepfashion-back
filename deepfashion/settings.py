8c7accceb59d2a3fa8b16db57bdb3908e4b8f08a
Dong Joo Lee <leedongjoo96@naver.com>
2019-12-14 20:19:09 +0900

    please work

--- deepfashion/settings.py
+++ deepfashion/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'deepfashion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "deepfashion",
+        "USER": "deepfashion",
+        "PASSWORD": "deepfashion",
+        "HOST": "deep-fashion.cviablub5osu.ap-northeast-2.rds.amazonaws.com",
+        "PORT": '5432',
     }
 }
 
