47c9cbad578c8f28194be3daf9039d1808d8c456
Dong Joo Lee <leedongjoo96@naver.com>
2019-12-20 20:12:14 +0900

    i think this fixes the cron thing

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,6 @@
 import datetime
 
 file_name = str(datetime.datetime.now().hour) + str(datetime.datetime.now().minute) + ".txt"
-output = open(file_name, "w+")
+output = open("../../opt/python/current/app/scripts/results/" + file_name, "w+")
 output.write(str(datetime.datetime.now()))
 output.close()
