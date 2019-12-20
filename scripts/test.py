c0721af30d9371ae2163b99b136dad44f121f6e8
Dong Joo Lee <leedongjoo96@naver.com>
2019-12-20 19:25:15 +0900

    cahnged test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,6 @@
 import datetime
 
 file_name = str(datetime.datetime.now().hour) + str(datetime.datetime.now().minute) + ".txt"
-output = open("results/" + file_name, "w")
+output = open("results/" + file_name, "w+")
 output.write(str(datetime.datetime.now()))
 output.close()
