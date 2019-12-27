40d5c8337222424ffba4ee6ac35bb0dd52b09236
Dong Joo Lee <leedongjoo96@naver.com>
2019-12-28 00:14:31 +0900

    debug boto

--- deepfashion/settings.py
+++ deepfashion/settings.py
@@ -139,8 +139,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
@@ -157,6 +155,9 @@ REST_FRAMEWORK = {
 
 MEDIA_URL = '/clothing/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
