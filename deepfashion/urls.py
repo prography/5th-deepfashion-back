08408a12c6c2fa05c59bee72f80d0681ae8bc9ba
Dong Joo Lee <leedongjoo96@naver.com>
2020-01-01 13:39:31 +0900

    changed index view to be in deepfashion and not clothes

--- deepfashion/urls.py
+++ deepfashion/urls.py
@@ -21,7 +21,7 @@ from weather import urls
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from clothes.views import index
+from .views import index
 
 schema_view = get_schema_view(
     openapi.Info(
